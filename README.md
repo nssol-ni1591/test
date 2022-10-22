8da8b15239f0bfca60d79107e7edd3860bc13fbe
Kou Gohdo <kutcha.papa@gmail.com>
2022-10-22 18:22:36 +0900

    2022-10-22 Generated gradle settings

--- README.md
+++ README.md
@@ -7,3 +7,21 @@
 
 .gitattributes
 /.gitignore merge=ours
+
+
+Steps to generate gradle project
+- gradle init
+    - libフォルダに環境が構築される
+- プロジェクトフォルダの直下に開発環境を生成する場合
+    - move files under lib 
+    - delete include('lib') in settings.gradle
+- ./gradlew eclipse ?
+- start eclipse
+- settings Peoject properties
+    - Project facets
+    - Targeted runtime
+    - Add id 'war' 
+- update build.gradle ... example
+    - Add "id 'war'" in plugins directive
+    - Comment out "implementation 'com.google.guava:guava:30.1.1-jre'"
+    - Add compileOnly fileTree(...) in dependencies directive
---
+++ src/main/java/Hello.java
@@ -0,0 +1,51 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+//@WebServlet({ "/hello", "/hello/*" })	... ok
+//@WebServlet({ "/*" })					... ok
+//@WebServlet({ "*" })					... サーブレットマッピング中に無効な <url-pattern> [*] があります
+//@WebServlet({ "hello/*" })			... サーブレットマッピング中に無効な <url-pattern> [hello/*] があります
+@WebServlet({ "/hello/*" })				//  つまり、URLを"/"始まりで記述すれば良いらしい
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void hello(PrintWriter out, HttpServletRequest request) {
+		out.println();
+//		out.println("<html><body>");
+//		out.println("<h1>Hello</h1>");
+		out.println("ContextPath: " + request.getContextPath());
+		out.println("RequestURI: " + request.getRequestURI());
+//		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain; charset=utf8");
+		hello(out, request);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

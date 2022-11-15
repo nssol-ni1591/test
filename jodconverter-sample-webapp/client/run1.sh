9bfda6e98a64a2232a3fb2b53bc5d807cb416fad
Kou Gohdo <kutcha.papa@gmail.com>
2022-11-15 10:28:47 +0900

    フォルダを組み替えたらよくわからなくなった

--- jodconverter-sample-webapp/client/run1.sh
+++
@@ -1,3 +0,0 @@
-CLASSPATH=`find test1-libs | tr '\n' '\\;'`
-CLASSPATH=classes\;${CLASSPATH}
-java -cp ${CLASSPATH} Test1 $*
--- jodconverter-sample-webapp/client/run3.sh
+++
@@ -1,3 +0,0 @@
-CLASSPATH=`find test2-libs | tr '\n' '\\;'`
-CLASSPATH=classes\;${CLASSPATH}
-java -cp ${CLASSPATH} Test3 $*

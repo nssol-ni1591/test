57a713d99f26650754d6f2a6bebbe58c9d045086
Kou Gohdo <gohdo.kou.2c3@jp.nssol.nipponsteel.com>
2023-03-14 07:42:26 +0900

    2023-03-14 Remove spring/gradle and spring/redis/src(dump)

--- spring/gradle/app/bin/main/scp.sh
+++
@@ -1,7 +0,0 @@
-[ ! -d WEB-INF ] && mkdir WEB-INF
-[ ! -d WEB-INF/classes ] && mkdir WEB-INF/classes
-[ ! -d WEB-INF/lib ] && mkdir WEB-INF/lib
-[ ! -f WEB-INF/lib/svfrclient.jar ] && cp -p svfrclient.jar WEB-INF/lib
-[ ! -f WEB-INF/classes/svfrclient.properties ] && cp -p svfrclient.properties WEB-INF/classes
-/opt/jdk-11/bin/jar -J-Dfile.encoding=utf8 cvf svf.war index.html WEB-INF
-scp svf.war svf@172.30.143.98:/tmp/sample
